a99d7a4cff7a88c0998af19a4d0b0d11af04d97b
tqhb2502 <tqhb2502@gmail.com>
2023-01-03 17:07:58 +0700

    add volume popup

--- src/main/MusicPlayer.java
+++ src/main/MusicPlayer.java
@@ -1,22 +1,38 @@
 package main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import util.Resources;
 
 public class MusicPlayer extends Application {
+	
+	private static Stage stage;
+	
+	public static void main(String[] args) {
+		Application.launch(MusicPlayer.class);
+	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-            
-            /// huy cmt
-			// huy tiep tuc cmt
-			// cmt thu 3 cua huy
-                        // thai cmtaa
 		
-		Parent view = FXMLLoader.load(getClass().getResource("/resource/view/Main.fxml"));
+		// prepare stage
+		MusicPlayer.stage = stage;
+		MusicPlayer.stage.setTitle("Music Player");
+		Image musicPlayerIcon = new Image(this.getClass().getResource(Resources.IMG + "Icon.png").toString());
+		MusicPlayer.stage.getIcons().add(musicPlayerIcon);
+		MusicPlayer.stage.setOnCloseRequest(event -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		
+		// load view
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource(Resources.FXML + "Main.fxml"));
+		Parent view = loader.load();
 
 		// Shows the scene containing the layout.
 		Scene scene = new Scene(view);
@@ -25,7 +41,7 @@ public class MusicPlayer extends Application {
 		stage.show();
 	}
 	
-	public static void main(String[] args) {
-		Application.launch(args);
+	public static Stage getStage() {
+		return stage;
 	}
 }
